3814b4c560a3696f4549e649ad6f8c7886e5d1a3
YuFeng <yufeng1684@163.com>
2023-06-20 10:06:51 +0800

    update SpringBootThymeleaf example

--- chapter_7/SpringBootThymeleaf/src/main/java/org/kmjs/springbootthymeleaf/controllers/PageController.java
+++ chapter_7/SpringBootThymeleaf/src/main/java/org/kmjs/springbootthymeleaf/controllers/PageController.java
@@ -1,6 +1,8 @@
 package org.kmjs.springbootthymeleaf.controllers;
 
+import org.kmjs.springbootthymeleaf.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,5 +12,17 @@ public class PageController {
     @RequestMapping(value="/welcome")
     public ModelAndView welcome() {
         return new ModelAndView("welcome");
+    }
+
+    @RequestMapping(value="/createuser")
+    public ModelAndView createUser() {
+        return new ModelAndView("createuser");
+    }
+
+    @RequestMapping(value="/save")
+    public ModelAndView save(@ModelAttribute User user){
+        ModelAndView modelAndView = new ModelAndView("save");
+        modelAndView.addObject("info", user.toString());
+        return modelAndView;
     }
 }
