a0a837f24342a9560ba5a92bc13b032fb8cfa9df
YuFeng <yufeng1684@163.com>
2023-06-22 00:20:14 +0800

    add SpringCloudEureka && SpringCloudServiceProvider example

---
+++ chapter_9/SpringCloudServiceProvider/src/test/java/orj/kmjs/springcloudserviceprovider/SpringCloudServiceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package orj.kmjs.springcloudserviceprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudServiceProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
