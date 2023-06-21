38f50fb79bcf88c249b2f4a743d93cdbe2e5dfb4
YuFeng <yufeng1684@163.com>
2023-06-21 11:37:41 +0800

    update SpringBootMybatis example

--- chapter_7/SpringBootMybatis/src/main/java/org/kmjs/springbootmybatis/controller/UserController.java
+++ chapter_7/SpringBootMybatis/src/main/java/org/kmjs/springbootmybatis/controller/UserController.java
@@ -1,8 +1,11 @@
 package org.kmjs.springbootmybatis.controller;
 
 import org.kmjs.springbootmybatis.domain.DomainUser;
+import org.kmjs.springbootmybatis.result.Result;
+import org.kmjs.springbootmybatis.result.ResultCode;
 import org.kmjs.springbootmybatis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,12 +15,26 @@ public class UserController {
     private UserService userService;
 
     @RequestMapping(value = "/{Id}", method = RequestMethod.GET)
-    public DomainUser getUser(@PathVariable("Id") int id){
-        return userService.getUser(id);
+    public Result<DomainUser> getUser(@PathVariable("Id") int id){
+        DomainUser domainUser =  userService.getUser(id);
+        Result<DomainUser> result = null;
+        if (domainUser != null) {
+            result = new Result<>(ResultCode.OK,domainUser);
+        } else {
+            result = new Result<>(ResultCode.Not_Found);
+        }
+        return result;
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public void addUser(@RequestBody DomainUser user){
-        userService.saveUser(user);
+    public Result<String> addUser(@RequestBody @Validated DomainUser user){
+        int ret = userService.saveUser(user);
+        Result<String> result = null;
+        if (ret == 1) {
+            result = new Result<>(ResultCode.OK);
+        } else {
+            result = new Result<>(ResultCode.Not_Found);
+        }
+        return result;
     }
 }
---
+++ chapter_7/SpringBootMybatis/src/main/java/org/kmjs/springbootmybatis/result/ResultCode.java
@@ -0,0 +1,5 @@
+package org.kmjs.springbootmybatis.result;
+
+public enum ResultCode {
+    OK,Bad_Request,Unauthorized,Not_Found,ERROR,Unavailable
+}
