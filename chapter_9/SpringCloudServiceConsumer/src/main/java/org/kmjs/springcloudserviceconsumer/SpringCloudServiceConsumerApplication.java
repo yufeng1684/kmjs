110ff751d2fe9b2f7077a66d3a9640d612edcd85
YuFeng <yufeng1684@163.com>
2023-06-24 22:44:10 +0800

    update Random loadbalance for SpringCloudServiceConsumer example

--- chapter_9/SpringCloudServiceConsumer/src/main/java/org/kmjs/springcloudserviceconsumer/SpringCloudServiceConsumerApplication.java
+++ chapter_9/SpringCloudServiceConsumer/src/main/java/org/kmjs/springcloudserviceconsumer/SpringCloudServiceConsumerApplication.java
@@ -1,14 +1,17 @@
 package org.kmjs.springcloudserviceconsumer;
 
+import org.kmjs.springcloudserviceconsumer.config.LoadBalancerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@LoadBalancerClient(value = "SERVICE-PROVIDER", configuration = LoadBalancerConfig.class)
 public class SpringCloudServiceConsumerApplication {
 
     public static void main(String[] args) {
